df124b44777e6dc03a160c72990ec48bd3d2e222
ElliottStrutton <76212111+ElliottStrutton@users.noreply.github.com>
2022-03-24 00:33:40 +0000

    Update main.py
    Added progress bar

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from tqdm import tqdm
+import time
 import urllib.request
+import os
 
+save_dir = 'exports'
 file = 'list.txt'
 links = []
 
 try:
     f = open(file, 'r').read()
+    print(f'The file {file} has been opened.')
 except FileNotFoundError:
     f = open(file, 'w').close()
     print(f'No file with the name {file} exists so one has been created')
 
 unformatted = f.split('\n')
-
 
 count = 0
 for x in unformatted:
@@ -19,7 +23,16 @@ for x in unformatted:
 
 links = list(dict.fromkeys(links))
 
+print('List formatted for download')
 
-for x in links:
-    name = x.split('/')
-    urllib.request.urlretrieve(x, f'exports/{name[-1]}')
+count = 0
+pbar = tqdm(range(0, len(links)))
+for x in pbar:
+    name = links[x].split('/')
+    pbar.set_description("Processing %s" % name[-1])
+    try:
+        urllib.request.urlretrieve(links[x], f'{save_dir}/{name[-1]}')
+    except FileNotFoundError:
+        os.mkdir(save_dir)
+        urllib.request.urlretrieve(links[x], f'{save_dir}/{name[-1]}')
+
